4f5c53d25b1d2d41875020891dded73c1ebc26ee
Danish <danishcv7@gmail.com>
2022-10-20 02:11:14 +0530

    do_now

--- template.py
+++ template.py
@@ -1,10 +1,12 @@
+
 import os
 from pathlib import Path
 import logging
 
-logging.basicConfig(level=logging.INFO,format='[%(asctime)s]:[%(message)s]')
+logging.basicConfig(level=logging.INFO,format='[%(asctime)s]:%(message)s: ')
 
-package_name = "deep_classifier"
+
+package_name = "deepClassifier"
 
 list_of_files = [
     ".github/workflows/.gitkeep",
@@ -13,31 +15,39 @@ list_of_files = [
     f"src/{package_name}/utils/__init__.py",
     f"src/{package_name}/config/__init__.py",
     f"src/{package_name}/pipeline/__init__.py",
-    f"src/{package_name}/entity/__init.py",
+    f"src/{package_name}/entity/__init__.py",
     f"src/{package_name}/constants/__init__.py",
-    "tests/__init__.py",
-    "tests/unit/__init__.py",
+    "tests/__init__.py"
+    "tests/unit/__init__.py"
+    "tests/integration/__init__.py"
     "configs/config.yaml",
-    "requirements.txt",
     "dvc.yaml",
     "params.yaml",
     "init_setup.sh",
-    "requirements_dev.txt",
+    "requirements.txt",
+    "requirements_dvc.txt",
     "setup.py",
-    "setup.cgf",
-    "setup.yaml"
+    "setup.cfg",
+    "pyproject.toml",
+    "tox.ini",
+    "research/trials.ipynb"
 ]
+
 
 for filepath in list_of_files:
     filepath = Path(filepath)
-    filedir,filename = os.path.split(filepath)
-    if filedir !="":
+    filedir, filename = os.path.split(filepath)
+    if filedir != "":
         os.makedirs(filedir,exist_ok=True)
         logging.info(f"Creating directory:{filedir} for file:{filename}")
 
-        if((not os.path.exists(filepath)) or os.path.getsize(filepath)==0):
-            with open(filepath,"w") as f:
-                pass
-                logging.info(f"Creating empty file:{filename}")
-        else:
-            logging.info(f"{filename} already exist")
+    if(not os.path.exists(filepath)) or (os.path.getsize(filepath)==0):
+        with open(filepath,"w") as f:
+            pass #create as empty file
+            logging.info(f"Creating empty file:{filepath}")
+
+    else:
+        logging.info(f"{filename} already exists")
+
+
+
