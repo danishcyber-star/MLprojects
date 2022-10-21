2d9c716fa48293224bc812e37dba9c28cad125d7
danishcyber-star <danishcv7@gmail.com>
2022-10-21 20:34:39 +0530

    init_setup 1st

--- init_setup.sh
+++ init_setup.sh
@@ -1,1 +1,8 @@
+echo [$(date)]: "START"
+echo [$(date)]: "Creating env with python 3.8 version"
 conda create --prefix ./env python=3.8 -y
+echo [$(date)]: "activate the environment"
+source activate ./env
+echo [$(date)]: "Installing DEV requirements"
+pip install -r requirements_dvc.txt
+echo [$(date)]: "END"
